4e3ee71395460bd8b794100804f6cb0643eba7d7
ThanhSan97 <sannguyen0907@gmail.com>
2022-10-31 23:36:25 +0700

    Chinh sua noi dung CS14

--- CS14_TinhKeThua/Program.cs
+++ CS14_TinhKeThua/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CS14_TinhKeThua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Tinh Ke Thua");
+        }
+    }
+}
