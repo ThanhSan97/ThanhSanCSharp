e8082664daa69b0787e69f022166382c3f9f322b
ThanhSan97 <sannguyen0907@gmail.com>
2022-11-02 23:38:26 +0700

    CS14 DONE

--- CS14_TinhKeThua/Program.cs
+++ CS14_TinhKeThua/Program.cs
@@ -1,11 +1,44 @@
 ﻿namespace CS14_TinhKeThua
-{
+{   class Animal
+    {
+        public int Legs{set; get;}
+        public float Weight{set; get;}
+        public void ShowLegs(){
+            System.Console.WriteLine(Legs);
+        }
+    }
+
+    class Cat : Animal
+    {
+        public string Sound;
+        public Cat()
+        {
+            this.Legs = 4;
+            this.Sound = "Meow Meow";
+        }
+        public void ShowSound()
+        {
+            System.Console.WriteLine(Sound);
+        }
+
+        public new void ShowLegs()
+        {
+            System.Console.WriteLine($"Loai meo co {Legs} chan");
+        }
+        public void ShowInfor()
+        {
+            
+        }
+    }
     class Program
     {
+        //Tinh ke thua - B:A
+
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Tinh Ke Thua");
-            //Vua moi comment
+            Cat c = new Cat();
+            c.ShowSound();
+            c.ShowLegs();
         }
     }
 }
