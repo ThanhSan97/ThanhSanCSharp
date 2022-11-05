0677a91418a57a9c6d540ce6ff1a00313a6e3fac
ThanhSan97 <sannguyen0907@gmail.com>
2022-11-05 16:50:26 +0700

    CS18 null va nullable

---
+++ CS18_NULL_va_Nullable/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CS18
+{
+    class Program
+    {   
+        class SinhVien
+        {
+            public void xinChao(){
+                System.Console.WriteLine("Xin chao");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            SinhVien sv = new SinhVien();
+            // if(sv != null){
+            //     sv.xinChao();
+            // }
+            sv?.xinChao();
+            //nullable
+            int? age;
+            age = null;
+            // age = 10;
+            if (age.HasValue)
+            {
+                int _age = age.Value;
+                System.Console.WriteLine(_age);   
+            } else 
+            {
+                System.Console.WriteLine("No value");
+            }
+        }
+    }
+}
