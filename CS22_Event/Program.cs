642733e3aef7a248856bf5c31b5fd7323171e4d8
ThanhSan97 <sannguyen0907@gmail.com>
2022-11-07 23:22:41 +0700

    CS22 event - Khong hieu gi het

--- CS22_Event/Program.cs
+++ CS22_Event/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CS22_Event
+{   /*Xây dựng những lớp có thể phát đi sự kiện
+    publisher -> class phat su kien
+    subscriber -> class nhan su kien
+    */
+    public delegate void SuKienNhapSo(int x);
+    
+    class Dulieu : EventArgs
+    {
+        public int data {get;set;}
+
+        public Dulieu (int x) => data = x;
+    }
+    // publisher
+    class userInput
+    {
+    // {   public event SuKienNhapSo sknhapso; // Co event => TU thuoc tinh tro thanh truong DL
+    //delegate chuyen dung cho su kien
+        public event EventHandler sknhapso; //delegate void Function(object? sender, EventaArgs args)
+        public void Input()
+        {   System.Console.WriteLine("Nhap so : ");
+            string s = Console.ReadLine();
+            int i = Int32.Parse(s);
+            sknhapso?.Invoke(this, new Dulieu(i));
+        }
+    }
+
+    //subscriber
+
+    class TinhCan
+    {   //DK sK
+        public void Sub(userInput input)
+        {
+            input.sknhapso += Can; //Khong duoc dung phep dan, chi duoc dung +=/-=
+        }
+        public void Can(object? sender,EventArgs args)
+        {   Dulieu dl = (Dulieu)args;
+            int i = dl.data;
+            System.Console.WriteLine($"Can bac 2 cua {i} la : {Math.Sqrt(i)}");
+        }
+    }
+
+    class BinhPhuong
+    {   //DK sK
+        public void Sub(userInput input)
+        {
+            input.sknhapso += TinhBinhPhuong;
+        }
+        public void TinhBinhPhuong(object? sender,EventArgs args)
+        {   Dulieu dl = (Dulieu)args;
+            int i = dl.data;
+            System.Console.WriteLine($"Binh phuong cua {i} la : {i*i}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            userInput us = new userInput();
+            // us.Input();
+
+            us.sknhapso += (sender, args) =>
+            {   Dulieu dl = (Dulieu)args;
+
+                System.Console.WriteLine($"Ban vua nhap so {dl.data}");
+            };
+            TinhCan tc = new TinhCan();
+            //Dang ki nhan su kien input
+            tc.Sub(us);
+
+            BinhPhuong bp = new BinhPhuong();
+            //Dang ki nhan su kien input cua lop binh phuong, huy dk sk cua lop tinhcan
+            bp.Sub(us);
+
+
+            us.Input();
+        }
+    }
+}
