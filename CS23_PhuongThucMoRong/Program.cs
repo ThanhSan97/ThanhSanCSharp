b897f9698146db2158b48e6bec2412951128ac2e
ThanhSan97 <sannguyen0907@gmail.com>
2022-11-10 22:24:22 +0700

    CS23 Extension Method

---
+++ CS23_PhuongThucMoRong/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CS23_Extension_method
+{   
+
+    static class Ex_method
+    {
+
+        //phai la phuong thuc tinh
+        public static void Show(this string s, ConsoleColor color) //Muon mo rong cho lop nao thi tham so dau tien la lop do =>String
+        {
+            Console.ForegroundColor = color;
+            System.Console.WriteLine(s);
+            Console.ResetColor();
+        }
+        
+        public static double BinhPhuong(this double a)
+        {
+            return a*a;
+        }
+    }
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            string s = "Xin chao cac ban";
+            //De s co the su dung phuong thuc show()
+            //Viet code trong phuong thuc tinh static
+            s.Show(ConsoleColor.Blue);
+            // "Xin".Show(ConsoleColor.Red);
+            // "Chao".Show(ConsoleColor.Green);
+            // "Cac".Show(ConsoleColor.Yellow);
+            // "Ban".Show(ConsoleColor.Cyan);
+            double a = 3.14;
+            System.Console.WriteLine($"Binh phuong cua {a} la : {a.BinhPhuong()}");
+        }
+    }
+}
